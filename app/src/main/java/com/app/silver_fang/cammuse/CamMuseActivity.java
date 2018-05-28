879c32310f55c6f6a2b731d8b21df6b1d2299f8b
silverfang94 <32561429+silverfang94@users.noreply.github.com>
2018-05-28 09:54:59 +0300

    Initial commit

---
+++ app/src/main/java/com/app/silver_fang/cammuse/CamMuseActivity.java
@@ -0,0 +1,13 @@
+package com.app.silver_fang.cammuse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CamMuseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cam_muse);
+    }
+}
